8137eda9a732e5c002df401d330805d1843f2150
crossroads30 <crossroads30@mail.ru>
2023-10-24 14:17:11 +0300

    fix: fix local host port to 3000

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localHost',
+    port: 3000,
+  }
 })
