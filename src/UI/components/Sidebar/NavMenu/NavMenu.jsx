2cded0c54be0c6c9732525ee6bb9be4422c484a9
crossroads30 <crossroads30@mail.ru>
2023-10-24 22:10:36 +0300

    feat: add nav menu

---
+++ src/UI/components/Sidebar/NavMenu/NavMenu.jsx
@@ -0,0 +1,56 @@
+import cl from './NavMenu.module.css'
+import { NavLink } from 'react-router-dom'
+
+const NavMenu = () => {
+	return (
+		<nav className={cl.navMenu}>
+			<ul className={cl.navList}>
+				<li>
+					<NavLink className={navData => (navData.isActive ? cl.active : cl.link)} to='/my-profile'>
+						My Profile
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						className={navData => (navData.isActive ? cl.active : cl.link)}
+						to='/my-channels'
+					>
+						My Channels
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className={navData => (navData.isActive ? cl.active : cl.link)} to='/my-friends'>
+						My Friends
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className={navData => (navData.isActive ? cl.active : cl.link)} to='/messages'>
+						Messages
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className={navData => (navData.isActive ? cl.active : cl.link)} to='/arcade'>
+						Arcade
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className={navData => (navData.isActive ? cl.active : cl.link)} to='/news'>
+						News
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className={navData => (navData.isActive ? cl.active : cl.link)} to='/resources'>
+						Resources
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className={navData => (navData.isActive ? cl.active : cl.link)} to='/settings'>
+						Settings
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default NavMenu
