9a9a34f1c13a527fb6e6f37d3191128ba44513b0
crossroads30 <crossroads30@mail.ru>
2023-10-24 15:49:26 +0300

    feat: add provider, add hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './styles/reset.css'
+import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import AppContainer from './AppContainer.jsx'
+import store from './BLL/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<HashRouter>
+			<Provider store={store}>
+				<AppContainer />
+			</Provider>
+		</HashRouter>
+	</React.StrictMode>
 )
