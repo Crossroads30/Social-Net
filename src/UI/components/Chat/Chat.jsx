3e28caf19ce2aacc2a85aeeb3b3e3db945a2219c
crossroads30 <crossroads30@mail.ru>
2023-10-25 21:57:50 +0300

    feat: add classes

--- src/UI/components/Chat/Chat.jsx
+++ src/UI/components/Chat/Chat.jsx
@@ -1,9 +1,8 @@
 import classNames from 'classnames'
-import cl from './Chat.module.css'
 
 const Chat = () => {
 	return (
-		<div className={classNames([cl.chat], 'box-wrapper')}>
+		<div className={classNames('box-wrapper', 'box-wrapper2')}>
 			<h2>chat</h2>
 		</div>
 	)
--- src/UI/components/Sidebar/Sidebar.jsx
+++ src/UI/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,14 @@
 import classNames from 'classnames'
-import cl from './SideBar.module.css'
 import Header from './Header/Header'
 import NavMenu from './NavMenu/NavMenu'
 import OnlineIndicator from './OnlineIndicator/OnlineIndicator'
 
 const Sidebar = () => {
 	return (
-		<aside className={classNames([cl.sidebar], 'box-wrapper')}>
-			<Header/>
-			<NavMenu/>
-			<OnlineIndicator/>
+		<aside className={classNames('box-wrapper', 'box-wrapper1')}>
+			<Header />
+			<NavMenu />
+			<OnlineIndicator />
 		</aside>
 	)
 }
