e8747e050ba5f3464e26dc1af3f8ce1e586e5f95
crossroads30 <crossroads30@mail.ru>
2023-10-25 20:34:41 +0300

    fix: delete extra state & action

--- src/UI/components/screens/MyProfile/MyPosts/MyPostsContainer.jsx
+++ src/UI/components/screens/MyProfile/MyPosts/MyPostsContainer.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import MyPosts from './MyPosts'
-import addPost from '../../../../../BLL/reducers/profile-reducer'
+// import { addPost } from '../../../../../BLL/reducers/profile-reducer'
 
 class MyPostsContainer extends React.Component {
 	componentDidMount() {}
 
 	render() {
-		return <MyPosts posts={this.props.posts} newPostMessageBody={this.props.newPostMessageBody} />
+		return <MyPosts posts={this.props.posts}  />
 	}
 }
 
 const mapStateToProps = state => {
 	return {
 		posts: state.profilePage.posts,
-		newPostMessageBody: state.profilePage.newPostMessageBody,
 	}
 }
 
-export default connect(mapStateToProps, {addPost})(MyPostsContainer)
+export default connect(mapStateToProps, {})(MyPostsContainer)
