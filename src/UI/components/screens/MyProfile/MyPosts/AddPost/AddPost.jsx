874e1ca44ae79ef08ac5e45461c6d83b3193ace7
crossroads30 <crossroads30@mail.ru>
2023-10-25 16:22:16 +0300

    feat: add component

---
+++ src/UI/components/screens/MyProfile/MyPosts/AddPost/AddPost.jsx
@@ -0,0 +1,11 @@
+import cl from './AddPost.module.css'
+
+const AddPost = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default AddPost
---
+++ src/UI/components/screens/MyProfile/MyPosts/AddPost/AddPostContainer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+
+class AddPostContainer extends React.Component {
+	componentDidMount() {
+    
+  }
+
+	render() { 
+return 
+ }
+}
+
+const mapStateToProps = state => {
+	return {
+
+	}
+}
+
+export default connect(mapStateToProps, {
+})(AddPostContainer)
