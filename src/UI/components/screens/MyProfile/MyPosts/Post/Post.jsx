33450a6ca26cdbe065ffec8b660d8ecc613603b1
crossroads30 <crossroads30@mail.ru>
2023-10-25 15:44:20 +0300

    feat: add post body

---
+++ src/UI/components/screens/MyProfile/MyPosts/Post/Post.jsx
@@ -0,0 +1,20 @@
+import cl from './Post.module.css'
+import defaultUserPhoto from '../../../../../../assets/icons/default-user-img.png'
+import heart from '../../../../../../assets/icons/icons8-heart-48.png'
+
+const Post = ({ text, likes }) => {
+	return (
+		<div className={cl.postWrapper}>
+			<div className={cl.post}>
+				<img src={defaultUserPhoto} alt='' />
+				<p>{text}</p>
+			</div>
+			<div className={cl.likes}>
+				<img src={heart} alt='heart' />
+				<span>{likes}</span>
+			</div>
+		</div>
+	)
+}
+
+export default Post
