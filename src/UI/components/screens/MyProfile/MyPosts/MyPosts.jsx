b94a86994b0bb17a7d8adeb21b54d8c730c2da65
crossroads30 <crossroads30@mail.ru>
2023-10-25 15:45:15 +0300

    feat: add posts with map method

--- src/UI/components/screens/MyProfile/MyPosts/MyPosts.jsx
+++ src/UI/components/screens/MyProfile/MyPosts/MyPosts.jsx
@@ -1,7 +1,14 @@
 import cl from './MyPosts.module.css'
+import Post from './Post/Post'
 
-const MyPosts = () => {
-	return <div>posts</div>
+const MyPosts = ({posts, newPostMessageBody}) => {
+	return (
+		<div>
+			{posts.map(post => (
+				<Post text={post.message} likes={post.likes} key={post.id} />
+			))}
+		</div>
+	)
 }
 
 export default MyPosts
