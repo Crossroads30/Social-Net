f6bd487cadab420dc00cf3c0d6885b448b003e21
crossroads30 <crossroads30@mail.ru>
2023-10-26 14:46:07 +0300

    feat: add wrapper for chat

--- src/UI/components/Chat/Chat.jsx
+++ src/UI/components/Chat/Chat.jsx
@@ -2,8 +2,10 @@ import classNames from 'classnames'
 
 const Chat = () => {
 	return (
-		<div className={classNames('box-wrapper', 'box-wrapper2')}>
-			<h2>chat</h2>
+		<div className='chat-wrapper'>
+			<div className={classNames('box-wrapper', 'box-wrapper2')}>
+				<h2>chat</h2>
+			</div>
 		</div>
 	)
 }
