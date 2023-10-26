cb1fad365b0c6007c86d7bb9ef03f73fb18558ba
crossroads30 <crossroads30@mail.ru>
2023-10-26 21:37:49 +0300

    feat: add condition for user name to show

--- src/UI/components/Sidebar/OnlineIndicator/OnlineIndicatorContainer.jsx
+++ src/UI/components/Sidebar/OnlineIndicator/OnlineIndicatorContainer.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { connect } from 'react-redux' 
+import { connect } from 'react-redux'
 import OnlineIndicator from './OnlineIndicator'
 
 class OnlineIndicatorContainer extends React.Component {
-	componentDidMount() {
-	}
+	componentDidMount() {}
 
 	render() {
 		return <OnlineIndicator userName={this.props.userName} />
@@ -12,10 +11,13 @@ class OnlineIndicatorContainer extends React.Component {
 }
 
 const mapStateToProps = state => {
-	return {
-		userName: state.profilePage.userProfile.fullName,
+	if (state.profilePage.userProfile !== null) {
+		return {
+			userName: state.profilePage.userProfile.fullName,
+		}
+	} return {
+		userName: 'User Name'
 	}
 }
 
-export default
-	connect(mapStateToProps, {})(OnlineIndicatorContainer)
+export default connect(mapStateToProps, {})(OnlineIndicatorContainer)
