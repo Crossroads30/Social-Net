df4bc1b1857148b8af2ffa9e831e714dc2c58447
crossroads30 <crossroads30@mail.ru>
2023-10-26 21:04:33 +0300

    feat: add hook, thunk callback

--- src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfoContainer.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfoContainer.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import ProfileInfo from './ProfileInfo'
+import { getUserProfile } from '../../../../../BLL/reducers/profile-reducer'
+import { compose } from 'redux'
+import { withRouter } from '../../../../../HOOKS/withRouter'
 
 class ProfileInfoContainer extends React.Component {
-	componentDidMount() {}
+	componentDidMount() {
+		let profileId = this.props.match.params.userId
+		if(!profileId) {
+			profileId = 30064
+		}
+		this.props.getUserProfile(profileId)
+	}
+
 
 	render() {
-		return <ProfileInfo />
+		return <ProfileInfo userProfile={this.props.userProfile} />
 	}
 }
 
 const mapStateToProps = state => {
-	return {}
+	return {
+		userProfile: state.profilePage.userProfile,
+	}
 }
 
-export default connect(mapStateToProps, {})(ProfileInfoContainer)
+export default compose(
+	connect(mapStateToProps, { getUserProfile }),
+	withRouter
+)(ProfileInfoContainer)
