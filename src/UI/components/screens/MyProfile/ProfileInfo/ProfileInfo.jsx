2718f4d76a9c62b271f58fcee3b413ab06cabc1c
crossroads30 <crossroads30@mail.ru>
2023-10-26 21:03:00 +0300

    feat: add userProfile data

--- src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfo.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfo.jsx
@@ -2,16 +2,19 @@ import cl from './ProfileInfo.module.css'
 import defaultUserImage from '../../../../../assets/icons/default-user-img.png'
 import SocialListContainer from './SocialList/SocialListContainer'
 
-const ProfileInfo = () => {
+const ProfileInfo = ({ userProfile }) => {
+	if (!userProfile) {
+		return <div> Loading...</div>
+	}
 	return (
 		<div className={cl.myProfileInfo}>
 			<div className={cl.profileImage}>
-				<img src={defaultUserImage} alt='default-user' />
+				<img src={userProfile.photos.small !== null ? userProfile.photos.small : defaultUserImage} alt='default-user' />
 			</div>
 			<div className={cl.infoWrapper}>
 				<div className={cl.info1}>
-					<h2>user name</h2>
-					<p>user status</p>
+					<h2>{userProfile.fullName}</h2>
+					{/* <p>{userProfile.status}</p> */}
 				</div>
 				<div className={cl.info2}>
 					<SocialListContainer />
