e8174b8e06faea767e6ec2237f332f4dd8f8f99e
crossroads30 <crossroads30@mail.ru>
2023-10-26 21:22:51 +0300

    feat: add prop from state

--- src/UI/components/Sidebar/OnlineIndicator/OnlineIndicatorContainer.jsx
+++ src/UI/components/Sidebar/OnlineIndicator/OnlineIndicatorContainer.jsx
@@ -1,32 +1,21 @@
-// import React from 'react'
-// import { connect } from 'react-redux'
-// import { getUserProfile } from '../../../../BLL/reducers/profile-reducer' 
-// import { compose } from 'redux'
-// import { withRouter } from '../../../../HOOKS/withRouter' 
-// import OnlineIndicator from './OnlineIndicator'
+import React from 'react'
+import { connect } from 'react-redux' 
+import OnlineIndicator from './OnlineIndicator'
 
-// class OnlineIndicatorContainer extends React.Component {
-// 	componentDidMount() {
-// 		// let profileId = this.props.match.params.userId
-// 		// if (!profileId) {
-// 		// 	profileId = 30064
-// 		// }
-// 		// this.props.getUserProfile(profileId)
-// 		// console.log(profileId)
-// 	}
+class OnlineIndicatorContainer extends React.Component {
+	componentDidMount() {
+	}
 
-// 	render() {
-// 		return <OnlineIndicator userProfile={this.props.userProfile} />
-// 	}
-// }
+	render() {
+		return <OnlineIndicator userName={this.props.userName} />
+	}
+}
 
-// const mapStateToProps = state => {
-// 	return {
-// 		userProfile: state.profilePage.userProfile,
-// 	}
-// }
+const mapStateToProps = state => {
+	return {
+		userName: state.profilePage.userProfile.fullName,
+	}
+}
 
-// export default compose(
-// 	connect(mapStateToProps, { getUserProfile }),
-// 	withRouter
-// )(OnlineIndicatorContainer)
+export default
+	connect(mapStateToProps, {})(OnlineIndicatorContainer)
