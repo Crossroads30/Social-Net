c51f79d0090763d0ddfcfd39d067a61acc2561ec
crossroads30 <crossroads30@mail.ru>
2023-10-26 16:48:30 +0300

    fea;: set state for userProfile with AC

--- src/BLL/reducers/profile-reducer.js
+++ src/BLL/reducers/profile-reducer.js
@@ -1,13 +1,15 @@
 
 const ADD_POST = 'profile/ADD_POST'
 const DELETE_POST = 'profile/DELETE_POST'
+const SET_USER_PROFILE = 'profile/SET_USER_PROFILE'
 
   let initialState = {
     posts: [
       {id:1, message:'Hello everybody!', likes: 2},
 			{id:2, message:"I'm trying really hard and I hope it's worth it!!!", likes: 12},
       {id:3, message:"Finally i will manage it!", likes: 12},
-    ]
+    ],
+		userProfile: null,
   }
 
 const profileReducer = (state = initialState, action) => {
@@ -27,6 +29,11 @@ const profileReducer = (state = initialState, action) => {
 				...state,
 				posts: state.posts.filter(post => post.id !== action.postId),
 			}
+		case SET_USER_PROFILE:
+			return {
+				...state,
+				userProfile: action.userProfile,
+			}
 		default:
 			return state
 	}
@@ -40,6 +47,10 @@ export const addPost = newPostMessage => ({
 export const deletePost = postId => ({
 	type: DELETE_POST,
 	postId,
+})
+export const setUserProfile = userProfile => ({
+	type: SET_USER_PROFILE,
+	userProfile,
 })
 
 export default profileReducer
