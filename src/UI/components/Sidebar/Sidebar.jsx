03c71a2b05d70dd4917d58e3ee60c5deb8f0322d
crossroads30 <crossroads30@mail.ru>
2023-10-26 21:24:12 +0300

    feat: add component

--- src/UI/components/Sidebar/Sidebar.jsx
+++ src/UI/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,14 @@
 import classNames from 'classnames'
 import Header from './Header/Header'
 import NavMenu from './NavMenu/NavMenu'
-import OnlineIndicator from './OnlineIndicator/OnlineIndicator'
+import OnlineIndicatorContainer from './OnlineIndicator/OnlineIndicatorContainer'
 
 const Sidebar = () => {
 	return (
 		<aside className={classNames('box-wrapper', 'box-wrapper1')}>
 			<Header />
 			<NavMenu />
-			{/* <OnlineIndicator /> */}
+			<OnlineIndicatorContainer />
 		</aside>
 	)
 }
