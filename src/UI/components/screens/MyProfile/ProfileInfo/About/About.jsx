9ad9c0394ef50cc15511ccb46d12e89e980d46b1
crossroads30 <crossroads30@mail.ru>
2023-10-27 15:46:11 +0300

    feat: add aboutMe fields

--- src/UI/components/screens/MyProfile/ProfileInfo/About/About.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/About/About.jsx
@@ -2,13 +2,29 @@ import cl from './About.module.css'
 
 const About = ({ userProfile }) => {
 	return (
-		<div className={cl.aboutWrapper}>
-			<div className={cl.aboutInfoWrapper}>
-				<div className={cl.aboutMeInfo}>info</div>
+		<>
+			{}
+			<div className={cl.aboutWrapper}>
+				<div className={cl.aboutInfoWrapper}>
+					<div className={cl.aboutMeInfo}>
+						<p>About me:</p>
+						<span>{userProfile.aboutMe}</span>
+						<p>My skills:</p>
+						<span>{userProfile.lookingForAJobDescription}</span>
+						<div className={cl.location}>
+							<span></span>
+							<p>Minsk, Belarus</p>
+						</div>
+						<div className={cl.job}>
+							<span></span>
+							<p> Work in "Altoros"</p>
+						</div>
+					</div>
+				</div>
+				<span className={cl.aboutButton}>About</span>
 			</div>
-			<span className={cl.aboutButton}>About</span>
-		</div>
-	) 
+		</>
+	)
 }
 
 export default About
