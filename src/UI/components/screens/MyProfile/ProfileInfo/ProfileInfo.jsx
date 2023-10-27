a093d5d3bef2b0e05004dca70cbfb73a0c9bde66
crossroads30 <crossroads30@mail.ru>
2023-10-27 14:41:09 +0300

    feat: add component

--- src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfo.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfo.jsx
@@ -2,30 +2,34 @@ import cl from './ProfileInfo.module.css'
 import defaultUserImage from '../../../../../assets/icons/default-user-img.png'
 import SocialListContainer from './SocialList/SocialListContainer'
 import UserStatus from './UserStatus/UserStatus'
+import About from './About/About'
 
 const ProfileInfo = ({ userProfile, userStatus, updateStatus }) => {
 	if (!userProfile) {
 		return <div> Loading...</div>
 	}
 	return (
-		<div className={cl.myProfileInfo}>
-			<div className={cl.profileImage}>
-				<img
-					src={userProfile.photos.small !== null ? userProfile.photos.small : defaultUserImage}
-					alt='default-user'
-				/>
-			</div>
-			<div className={cl.infoWrapper}>
-				<div className={cl.info1}>
-					<h2>{userProfile.fullName}</h2>
-					<UserStatus userStatus={userStatus} updateStatus={updateStatus} />
+		<>
+			<div className={cl.myProfileInfo}>
+				<div className={cl.profileImage}>
+					<img
+						src={userProfile.photos.small !== null ? userProfile.photos.small : defaultUserImage}
+						alt='default-user'
+					/>
 				</div>
-				<div className={cl.info2}>
-					<SocialListContainer />
-					<p>logout</p>
+				<div className={cl.infoWrapper}>
+					<div className={cl.info1}>
+						<h2>{userProfile.fullName}</h2>
+						<UserStatus userStatus={userStatus} updateStatus={updateStatus} />
+					</div>
+					<div className={cl.info2}>
+						<SocialListContainer />
+						<p>logout</p>
+					</div>
 				</div>
 			</div>
-		</div>
+			<About userProfile={userProfile} />
+		</>
 	)
 }
 
