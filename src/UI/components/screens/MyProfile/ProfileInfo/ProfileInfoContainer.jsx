96fd8390019882d84c693810973072646e29c722
crossroads30 <crossroads30@mail.ru>
2023-10-27 10:43:03 +0300

    feat: add getUserStatus callback

--- src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfoContainer.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfoContainer.jsx
@@ -1,32 +1,33 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import ProfileInfo from './ProfileInfo'
-import { getUserProfile } from '../../../../../BLL/reducers/profile-reducer'
+import { getUserProfile, getUserStatus } from '../../../../../BLL/reducers/profile-reducer'
 import { compose } from 'redux'
 import { withRouter } from '../../../../../HOOKS/withRouter'
 
 class ProfileInfoContainer extends React.Component {
 	componentDidMount() {
 		let profileId = this.props.match.params.userId
-		if(!profileId) {
+		if (!profileId) {
 			profileId = 30064
 		}
 		this.props.getUserProfile(profileId)
+		this.props.getUserStatus(profileId)
 	}
 
-
 	render() {
-		return <ProfileInfo userProfile={this.props.userProfile} />
+		return <ProfileInfo userProfile={this.props.userProfile} userStatus={this.props.userStatus} />
 	}
 }
 
 const mapStateToProps = state => {
 	return {
 		userProfile: state.profilePage.userProfile,
+		userStatus: state.profilePage.userStatus,
 	}
 }
 
 export default compose(
-	connect(mapStateToProps, { getUserProfile }),
+	connect(mapStateToProps, { getUserProfile, getUserStatus }),
 	withRouter
 )(ProfileInfoContainer)
