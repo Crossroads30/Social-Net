bfbc0c9839f1799f38786cdff8c1cf965620c9b6
crossroads30 <crossroads30@mail.ru>
2023-10-27 10:43:38 +0300

    feat: add component UserStatus

--- src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfo.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfo.jsx
@@ -1,20 +1,24 @@
 import cl from './ProfileInfo.module.css'
 import defaultUserImage from '../../../../../assets/icons/default-user-img.png'
 import SocialListContainer from './SocialList/SocialListContainer'
+import UserStatus from './UserStatus/UserStatus'
 
-const ProfileInfo = ({ userProfile }) => {
+const ProfileInfo = ({ userProfile, userStatus }) => {
 	if (!userProfile) {
 		return <div> Loading...</div>
 	}
 	return (
 		<div className={cl.myProfileInfo}>
 			<div className={cl.profileImage}>
-				<img src={userProfile.photos.small !== null ? userProfile.photos.small : defaultUserImage} alt='default-user' />
+				<img
+					src={userProfile.photos.small !== null ? userProfile.photos.small : defaultUserImage}
+					alt='default-user'
+				/>
 			</div>
 			<div className={cl.infoWrapper}>
 				<div className={cl.info1}>
 					<h2>{userProfile.fullName}</h2>
-					{/* <p>{userProfile.status}</p> */}
+					<UserStatus userStatus={userStatus} />
 				</div>
 				<div className={cl.info2}>
 					<SocialListContainer />
