f3e35d0679fb0374d9e75762bd53da4e733939e8
crossroads30 <crossroads30@mail.ru>
2023-10-28 14:57:10 +0300

    refactor: add mapping for social list

--- src/UI/components/screens/MyProfile/ProfileInfo/SocialList/SocialList.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/SocialList/SocialList.jsx
@@ -1,73 +1,33 @@
 import cl from './SocialList.module.css'
-import facebookImage from '../../../../../../assets/icons/icons8-facebook-48.png'
-import twitterImage from '../../../../../../assets/icons/icons8-twitter-48.png'
-import gitImage from '../../../../../../assets/icons/icons8-github-logo-48.png'
-import instagramImage from '../../../../../../assets/icons/icons8-instagram-48.png'
-import youTubeImage from '../../../../../../assets/icons/icons8-youtube-48.png'
-import websiteImage from '../../../../../../assets/icons/icons8-web-48.png'
-import vkImage from '../../../../../../assets/icons/icons8-vk-48.png'
-import mainLinkImage from '../../../../../../assets/icons/icons8-link-48.png'
+//all imports below are using as key names for img src from 'userProfile.contacts' while mapping it
+import facebook from '../../../../../../assets/icons/icons8-facebook-48.png'
+import twitter from '../../../../../../assets/icons/icons8-twitter-48.png'
+import github from '../../../../../../assets/icons/icons8-github-logo-48.png'
+import instagram from '../../../../../../assets/icons/icons8-instagram-48.png'
+import youtube from '../../../../../../assets/icons/icons8-youtube-48.png'
+import website from '../../../../../../assets/icons/icons8-web-48.png'
+import vk from '../../../../../../assets/icons/icons8-vk-48.png'
+import mainLink from '../../../../../../assets/icons/icons8-link-48.png'
+import { useMemo } from 'react'
 
-
-const SocialList = () => {
-  return (
+const SocialList = ({ userProfile }) => {
+	return (
 		<ul className={cl.socialList}>
-			<li>
-				<div>
-					<a href='http://facebook.com'>
-						<img src={facebookImage} alt='facebook' />
-					</a>
-				</div>
-			</li>
-			<li>
-				<div>
-					<a href='http://facebook.com'>
-						<img src={websiteImage} alt='facebook' />
-					</a>
-				</div>
-			</li>
-			<li>
-				<div>
-					<a href='http://facebook.com'>
-						<img src={vkImage} alt='facebook' />
-					</a>
-				</div>
-			</li>
-			<li>
-				<div>
-					<a>
-						<img src={twitterImage} alt='twitter' />
-					</a>
-				</div>
-			</li>
-			<li>
-				<div>
-					<a>
-						<img src={instagramImage} alt='instagram' />
-					</a>
-				</div>
-			</li>
-			<li>
-				<div>
-					<a>
-						<img src={youTubeImage} alt='youtube' />
-					</a>
-				</div>
-			</li>
-			<li>
-				<div>
-					<a>
-						<img src={gitImage} alt='github' />
-					</a>
-				</div>
-			</li>
-			<li>
-				<div>
-					<a>
-						<img src={mainLinkImage} alt='github' />
-					</a>
-				</div>
-			</li>
+		{/* for preventing a dabble rendering  use 'useMemo()' method */}
+			{useMemo(() => {
+				return Object.entries(userProfile.contacts).map(([key, value]) => {
+					return (
+						<li key={key}>
+							<div>
+								<a href={`http://${value}`}>
+									{/* for string(from 'key') to variable(import name) use 'eval() method!!!' */}
+									<img src={eval(key)} alt={key} />
+								</a>
+							</div>
+						</li>
+					)
+				})
+			})}
 		</ul>
 	)
 }
