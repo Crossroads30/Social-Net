3d54124dc9685dbc30575a21e2c775743d098c88
crossroads30 <crossroads30@mail.ru>
2023-10-28 19:02:12 +0300

    fix: fix component

--- src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfo.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfo.jsx
@@ -1,8 +1,9 @@
 import cl from './ProfileInfo.module.css'
 import defaultUserImage from '../../../../../assets/icons/default-user-img.png'
-import SocialList from './SocialList/SocialList'
+// import SocialList from './SocialList/SocialList'
 import UserStatus from './UserStatus/UserStatus'
 import About from './About/About'
+import EditSocialList from './SocialList/EditSocialList'
 
 const ProfileInfo = ({ userProfile, userStatus, updateStatus, userLocation, workAt }) => {
 	if (!userProfile) {
@@ -13,22 +14,37 @@ const ProfileInfo = ({ userProfile, userStatus, updateStatus, userLocation, work
 			<div className={cl.myProfileInfo}>
 				<div className={cl.profileImage}>
 					<img
-						src={userProfile.photos.small !== null ? userProfile.photos.small : defaultUserImage}
+						src={
+							userProfile.photos.small !== null
+								? userProfile.photos.small
+								: defaultUserImage
+						}
 						alt='default-user'
 					/>
 				</div>
 				<div className={cl.infoWrapper}>
 					<div className={cl.info1}>
 						<h2>{userProfile.fullName}</h2>
-						<UserStatus userStatus={userStatus} updateStatus={updateStatus} />
+						<UserStatus
+							userStatus={userStatus}
+							updateStatus={updateStatus}
+						/>
 					</div>
 					<div className={cl.info2}>
-						<SocialList userProfile={userProfile} userLocation={userLocation} workAt={workAt} />
+						<EditSocialList
+							userProfile={userProfile}
+							userLocation={userLocation}
+							workAt={workAt}
+						/>
 						<p>logout</p>
 					</div>
 				</div>
 			</div>
-			<About userProfile={userProfile} userLocation={userLocation} workAt={workAt} />
+			<About
+				userProfile={userProfile}
+				userLocation={userLocation}
+				workAt={workAt}
+			/>
 		</>
 	)
 }
