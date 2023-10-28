cdb938e843fd5a442e41dadc90d73162540aa5be
crossroads30 <crossroads30@mail.ru>
2023-10-28 21:15:56 +0300

    feat: add AC & thunk update profile

--- src/BLL/reducers/profile-reducer.js
+++ src/BLL/reducers/profile-reducer.js
@@ -4,11 +4,16 @@ const ADD_POST = 'profile/ADD_POST'
 const DELETE_POST = 'profile/DELETE_POST'
 const SET_USER_PROFILE = 'profile/SET_USER_PROFILE'
 const SET_USER_STATUS = 'profile/SET_USER_STATUS'
+const UPDATE_USER_PROFILE = 'profile/UPDATE_USER_PROFILE'
 
 let initialState = {
 	posts: [
 		{ id: 1, message: 'Hello everybody!', likes: 2 },
-		{ id: 2, message: "I'm trying really hard and I hope it's worth it!!!", likes: 12 },
+		{
+			id: 2,
+			message: "I'm trying really hard and I hope it's worth it!!!",
+			likes: 12,
+		},
 		{ id: 3, message: 'Finally i will manage it!', likes: 12 },
 	],
 	userProfile: null,
@@ -44,6 +49,11 @@ const profileReducer = (state = initialState, action) => {
 				...state,
 				userStatus: action.userStatus,
 			}
+		case UPDATE_USER_PROFILE:
+			return {
+				...state,
+				updateUserProfile: action.updateUserProfile,
+			}
 		default:
 			return state
 	}
@@ -66,19 +76,48 @@ export const setUserStatus = userStatus => ({
 	type: SET_USER_STATUS,
 	userStatus,
 })
+export const updateUserProfile = userProfile => ({
+	type: UPDATE_USER_PROFILE,
+	userProfile,
+})
 
 //ThunkCreators:
 export const getUserProfile = profileId => async dispatch => {
-	const response = await profileApi.getProfile(profileId)
-	dispatch(setUserProfile(response.data))
+	try {
+		const response = await profileApi.getProfile(profileId)
+		dispatch(setUserProfile(response.data))
+	} catch (error) {
+		console.log(error)
+	}
 }
+
 export const getUserStatus = profileId => async dispatch => {
-	const response = await profileApi.getStatus(profileId)
-	dispatch(setUserStatus(response.data))
+	try {
+		const response = await profileApi.getStatus(profileId)
+		dispatch(setUserStatus(response.data))
+	} catch (error) {
+		console.log(error)
+	}
 }
+
 export const getUpdateUserStatus = userStatus => async dispatch => {
-	const response = await profileApi.updateStatus(userStatus)
-	response.data.resultCode === 0 && dispatch(setUserStatus(userStatus))
+	try {
+		const response = await profileApi.updateStatus(userStatus)
+		response.data.resultCode === 0 &&
+			dispatch(setUserStatus(userStatus))
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+export const getUpdateUserProfile = userProfile => async dispatch => {
+	try {
+		const response = await profileApi.updateProfile(userProfile)
+		response.data.resultCode === 0 &&
+			dispatch(setUserStatus(userProfile))
+	} catch (error) {
+		console.log(error)
+	}
 }
 
 export default profileReducer
