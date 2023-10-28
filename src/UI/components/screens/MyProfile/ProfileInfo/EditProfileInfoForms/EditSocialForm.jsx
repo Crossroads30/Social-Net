e624af47a31c3c49bb3436d8c55dfe13b85ec52a
crossroads30 <crossroads30@mail.ru>
2023-10-28 17:34:54 +0300

    feat: create social form

--- src/UI/components/screens/MyProfile/ProfileInfo/EditProfileInfoForms/EditSocialForm.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/EditProfileInfoForms/EditSocialForm.jsx
@@ -1,12 +1,36 @@
 import { reduxForm } from 'redux-form'
 import cl from './EditProfileInfoForms.module.css'
-import { createField } from '../../../../common/form-tools/FormControls/CreateField'  
+import { createField } from '../../../../common/form-tools/FormControls/CreateField'
+import {
+	maxLengthCreator,
+	required,
+} from '../../../../common/form-tools/validators/validators'
 
-const EditSocialForm = ({ handleSubmit }) => {
+const maxLength20 = maxLengthCreator(20)
+
+const EditSocialForm = ({ handleSubmit, userProfile, error }) => {
+
 	return (
-		<div>
+		<div className={cl.socialForm}>
 			<form onSubmit={handleSubmit}>
-				{}
+				<ul className={cl.socials}>
+					{Object.keys(userProfile.contacts).map(key => {
+						return (
+							<li key={key}>
+								<b>{key}:</b>
+								{createField(
+									null,
+									key,
+									'contacts.' + key,
+									[maxLength20],
+									'input',
+									{}
+								)}
+							</li>
+						)
+					})}
+				</ul>
+				{error && <div className={cl.formSummaryError}>{error}</div>}
 			</form>
 		</div>
 	)
