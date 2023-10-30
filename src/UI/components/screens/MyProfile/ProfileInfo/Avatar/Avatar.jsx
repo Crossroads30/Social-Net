6105f16b0816c72ee56956830d62f1cfdf23f87f
crossroads30 <crossroads30@mail.ru>
2023-10-30 22:08:17 +0300

    feat: add props & photo select func

--- src/UI/components/screens/MyProfile/ProfileInfo/Avatar/Avatar.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/Avatar/Avatar.jsx
@@ -2,7 +2,12 @@ import cl from '../../ProfileInfo/ProfileInfo.module.css'
 import defaultUserImage from '../../../../../../assets/icons/default-user-img.png'
 import photoIcon from '../../../../../../assets/icons/icons8-camera-48.png'
 
-const Avatar = ({ userProfile }) => {
+const Avatar = ({ userProfile, savePhoto }) => {
+
+  	const onAvatarSelected = event => {
+			event.target.files.length && savePhoto(event.target.files[0]) //условие - если фото есть(event.target.files.length), то тогда передаем его в props
+		}
+
 	return (
 		<>
 			<div className={cl.profileImage}>
@@ -10,6 +15,7 @@ const Avatar = ({ userProfile }) => {
 			</div>
 			<div className={cl.photoIcon}>
 				<img src={photoIcon} alt='photo-icon' />
+				<input type={'file'} onChange={onAvatarSelected}></input>
 			</div>
 		</>
 	)
