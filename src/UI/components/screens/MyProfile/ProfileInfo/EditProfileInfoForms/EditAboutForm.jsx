cacebec33b7ba932fb4b30546258e8662201a315
crossroads30 <crossroads30@mail.ru>
2023-10-30 12:01:37 +0300

    feat: add form

--- src/UI/components/screens/MyProfile/ProfileInfo/EditProfileInfoForms/EditAboutForm.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/EditProfileInfoForms/EditAboutForm.jsx
@@ -4,9 +4,64 @@ import cl from './EditProfileInfoForms.module.css'
 const EditAboutForm = ({ handleSubmit }) => {
 	return (
 		<div>
-			<form onSubmit={handleSubmit}>
-        
-      </form>
+			<form
+				onSubmit={handleSubmit}
+				className={cl.userNameForm}
+			>
+				<div className={cl.submit}>
+					<CloseButton
+						closeWindow={deactivateEditUser}
+					/>
+				</div>
+				{createField(
+					null,
+					'User Name',
+					'aboutMe',
+					[],
+					'textarea',
+					{}
+				)}
+				{createField(
+					null,
+					'User Name',
+					'lookingForAJob',
+					[],
+					'checkbox',
+					{}
+				)}
+				{createField(
+					null,
+					'User Name',
+					'lookingForAJobDescription',
+					[],
+					'textarea',
+					{}
+				)}
+				{createField(
+					null,
+					'User Name',
+					'userLocation',
+					[],
+					'input',
+					{}
+				)}
+				{createField(
+					null,
+					'User Name',
+					'workAt',
+					[],
+					'input',
+					{}
+				)}
+				<div className={cl.submit}>
+					<SubmitButton name={'Submit'} />
+				</div>
+				{error && (
+					<div className={cl.formSummaryError}>
+						{error}
+					</div>
+				)}
+			</form>
 		</div>
 	)
 }
