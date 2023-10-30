b1149546aaa601db23d4b34d50d11bbfd66efbdc
crossroads30 <crossroads30@mail.ru>
2023-10-30 10:55:32 +0300

    feat: add redux form for edit user name

--- src/UI/components/screens/MyProfile/ProfileInfo/EditProfileInfoForms/EditUserNameForm.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/EditProfileInfoForms/EditUserNameForm.jsx
@@ -1,10 +1,37 @@
 import { reduxForm } from 'redux-form'
 import cl from './EditProfileInfoForms.module.css'
+import { createField } from '../../../../common/form-tools/FormControls/CreateField'
+import SubmitButton from '../../../../common/buttons/SubmitButton'
+import { maxLengthCreator } from '../../../../common/form-tools/validators/validators'
+import CloseButton from '../../../../common/buttons/CloseButton'
 
-const EditUserNameForm = ({ handleSubmit }) => {
+const maxLength20 = maxLengthCreator(20)
+
+const EditUserNameForm = ({
+	handleSubmit,
+	error,
+	userProfile,
+	deactivateEditUser,
+}) => {
 	return (
 		<div>
-			<form onSubmit={handleSubmit}></form>
+			<form onSubmit={handleSubmit} className={cl.userNameForm}>
+				<div className={cl.submit}>
+					<CloseButton closeWindow={deactivateEditUser} />
+				</div>
+				{createField(
+					null,
+					'User Name',
+					'fullName',
+					[maxLength20],
+					'input',
+					{}
+				)}
+				<div className={cl.submit}>
+					<SubmitButton name={'Submit'} />
+				</div>
+				{error && <div className={cl.formSummaryError}>{error}</div>}
+			</form>
 		</div>
 	)
 }
