93bdbcf8cdc8c0ddab6452618800258672619d22
crossroads30 <crossroads30@mail.ru>
2023-10-30 15:47:30 +0300

    feat: add fields

--- src/UI/components/screens/MyProfile/ProfileInfo/EditProfileInfoForms/EditAboutForm.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/EditProfileInfoForms/EditAboutForm.jsx
@@ -1,66 +1,26 @@
 import { reduxForm } from 'redux-form'
 import cl from './EditProfileInfoForms.module.css'
+import CloseButton from '../../../../common/buttons/CloseButton'
+import { createField } from '../../../../common/form-tools/FormControls/CreateField'
+import SubmitButton from '../../../../common/buttons/SubmitButton'
 
-const EditAboutForm = ({ handleSubmit }) => {
+const EditAboutForm = ({ handleSubmit, closeEditWindow }) => {
 	return (
 		<div>
-			<form
-				onSubmit={handleSubmit}
-				className={cl.userNameForm}
-			>
+			<form onSubmit={handleSubmit} className={cl.aboutForm}>
 				<div className={cl.submit}>
-					<CloseButton
-						closeWindow={deactivateEditUser}
-					/>
+					<CloseButton closeWindow={closeEditWindow} />
 				</div>
-				{createField(
-					null,
-					'User Name',
-					'aboutMe',
-					[],
-					'textarea',
-					{}
-				)}
-				{createField(
-					null,
-					'User Name',
-					'lookingForAJob',
-					[],
-					'checkbox',
-					{}
-				)}
-				{createField(
-					null,
-					'User Name',
-					'lookingForAJobDescription',
-					[],
-					'textarea',
-					{}
-				)}
-				{createField(
-					null,
-					'User Name',
-					'userLocation',
-					[],
-					'input',
-					{}
-				)}
-				{createField(
-					null,
-					'User Name',
-					'workAt',
-					[],
-					'input',
-					{}
-				)}
+				{createField(null, 'About me', 'aboutMe', [], 'textarea', {})}
+				<div>
+					{createField(cl.check, null, 'lookingForAJob', [], 'input', { type: 'checkbox' }, 'Looking For A Job ?')}
+					<p>Looking For A Job ?</p>
+				</div>
+				{createField(null, 'looking For A Job Description', 'lookingForAJobDescription', [], 'textarea', {})}
 				<div className={cl.submit}>
 					<SubmitButton name={'Submit'} />
 				</div>
-				{error && (
-					<div className={cl.formSummaryError}>
-						{error}
-					</div>
-				)}
+				{/* {error && <div className={cl.formSummaryError}>{error}</div>} */}
 			</form>
 		</div>
 	)
