ca5d903ad6a5dbfa2f870bd75865290214e3a6d6
crossroads30 <crossroads30@mail.ru>
2023-10-30 15:46:44 +0300

    feat: add redux form

--- src/UI/components/screens/MyProfile/ProfileInfo/About/About.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/About/About.jsx
@@ -3,8 +3,9 @@ import cl from './About.module.css'
 import classNames from 'classnames'
 import CloseButton from '../../../../common/buttons/CloseButton'
 import EditButton from '../../../../common/buttons/EditButton'
+import { EditAboutReduxForm } from '../EditProfileInfoForms/EditAboutForm'
 
-const About = ({ userProfile, userLocation, workAt }) => {
+const About = ({ userProfile }) => {
 	const [about, setAbout] = useState(false)
 
 	const openAbout = () => {
@@ -12,48 +13,45 @@ const About = ({ userProfile, userLocation, workAt }) => {
 	}
 	const closeWindow = () => {
 		setAbout(false)
+		setEditAbout(false)
+	}
+	const [editAbout, setEditAbout] = useState(false)
+
+	const activateEditAbout = () => {
+		setEditAbout(true)
+	}
+	const closeEditWindow = () => {
+		setEditAbout(false)
 	}
 
 	return (
 		<>
 			<div className={cl.aboutWrapper}>
-				<span
-					onClick={openAbout}
-					className={
-						!about
-							? cl.aboutButton
-							: classNames(cl.aboutButton, cl.aboutButtonDisable)
-					}
-				>
+				<span onClick={openAbout} className={!about ? cl.aboutButton : classNames(cl.aboutButton, cl.aboutButtonDisable)}>
 					About
 				</span>
-				<div
-					className={
-						!about
-							? cl.aboutInfoWrapper
-							: classNames(cl.aboutInfoWrapper, cl.aboutInfoWrapperActive)
-					}
-				>
+				<div className={!about ? cl.aboutInfoWrapper : classNames(cl.aboutInfoWrapper, cl.aboutInfoWrapperActive)}>
 					<div className={cl.close}>
 						<CloseButton closeWindow={closeWindow} />
 					</div>
-					<div className={cl.aboutMeInfo}>
-						<p>About me:</p>
-						<span>{userProfile.aboutMe}</span>
-						<p>My skills:</p>
-						<span>{userProfile.lookingForAJobDescription}</span>
-						<div className={cl.location}>
-							<span></span>
-							<p>{userLocation}</p>
-						</div>
-						<div className={cl.job}>
-							<span></span>
-							<p>{workAt}</p>
-						</div>
-					</div>
-					<div className={cl.editAbout}>
-						<EditButton />
-					</div>
+					{!editAbout && (
+						<>
+							<div className={cl.aboutMeInfo}>
+								<div>
+									<p>About me:</p>
+									<span>{userProfile.aboutMe}</span>
+								</div>
+								<div>
+									<p>My skills:</p>
+									<span>{userProfile.lookingForAJobDescription}</span>
+								</div>
+							</div>
+							<div className={cl.editAboutButton}>
+								<EditButton goToEditMode={activateEditAbout} />
+							</div>
+						</>
+					)}
+					{editAbout && <EditAboutReduxForm closeEditWindow={closeEditWindow} />}
 				</div>
 			</div>
 		</>
