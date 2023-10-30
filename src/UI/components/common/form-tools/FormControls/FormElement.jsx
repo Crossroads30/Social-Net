1e53a5d0e5d7e4cdd3eafdcc4d8f099d2a8df699
crossroads30 <crossroads30@mail.ru>
2023-10-30 15:45:09 +0300

    fix: del div

--- src/UI/components/common/form-tools/FormControls/FormElement.jsx
+++ src/UI/components/common/form-tools/FormControls/FormElement.jsx
@@ -1,17 +1,13 @@
 import cl from './FormElement.module.css'
 import classNames from 'classnames'
 
- export const FormElement = ({ input, meta: { touched, error }, ...props }) => {
+export const FormElement = ({ input, meta: { touched, error }, ...props }) => {
 	const hasError = touched && error
 
 	return (
 		<div className={classNames([cl.formControl], hasError ? [cl.error] : '')}>
-			<div>
-				<props.element_type {...input} {...props} />
-				{hasError && <span>{error}</span>}
-			</div>
+			<props.element_type {...input} {...props} />
+			{hasError && <span>{error}</span>}
 		</div>
 	)
 }
-
-
