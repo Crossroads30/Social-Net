0553e8b1129659e38860792b0edc6a3acd832416
crossroads30 <crossroads30@mail.ru>
2023-10-30 09:24:30 +0300

    feat: add edit-btn component

--- src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfo.jsx
+++ src/UI/components/screens/MyProfile/ProfileInfo/ProfileInfo.jsx
@@ -4,6 +4,7 @@ import defaultUserImage from '../../../../../assets/icons/default-user-img.png'
 import UserStatus from './UserStatus/UserStatus'
 import About from './About/About'
 import EditSocialList from './SocialList/EditSocialList'
+import EditButton from '../../../common/buttons/EditButton'
 
 const ProfileInfo = ({
 	userProfile,
@@ -31,11 +32,11 @@ const ProfileInfo = ({
 				</div>
 				<div className={cl.infoWrapper}>
 					<div className={cl.info1}>
-						<h2>{userProfile.fullName}</h2>
-						<UserStatus
-							userStatus={userStatus}
-							updateStatus={updateStatus}
-						/>
+						<div className={cl.fullName}>
+							<h2>{userProfile.fullName}</h2>
+							<EditButton />
+						</div>
+							<UserStatus userStatus={userStatus} updateStatus={updateStatus} />
 					</div>
 					<div className={cl.info2}>
 						<EditSocialList
