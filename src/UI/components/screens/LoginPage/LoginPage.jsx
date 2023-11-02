3679cadc1b2892a40d85f823bb7e59f6f4aed184
crossroads30 <crossroads30@mail.ru>
2023-11-02 14:27:18 +0300

    feat: add form data for submit to send, add props

--- src/UI/components/screens/LoginPage/LoginPage.jsx
+++ src/UI/components/screens/LoginPage/LoginPage.jsx
@@ -1,12 +1,21 @@
+import { Navigate } from 'react-router-dom'
 import cl from './LoginPage.module.css'
 import { LoginPageReduxForm } from './loginPageForm'
 
-const LoginPage = () => {
+const LoginPage = ({ login, isAuth }) => {
+
+	const addLoginData = formData => {
+		login(formData.email, formData.password, formData.rememberMe, formData.captcha)
+	}
+
+	if (isAuth) {
+		return <Navigate to='/my-profile' />
+	}
 	return (
 		<>
-			<LoginPageReduxForm />
+			<LoginPageReduxForm onSubmit={addLoginData} isAuth={isAuth} />
 		</>
-	) 
+	)
 }
 
 export default LoginPage
