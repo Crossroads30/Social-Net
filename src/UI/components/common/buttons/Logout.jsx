ddb67c478bb694a98b44111ee608f109117ae2af
crossroads30 <crossroads30@mail.ru>
2023-11-02 15:14:25 +0300

    feat: add prop, add onClock

--- src/UI/components/common/buttons/Logout.jsx
+++ src/UI/components/common/buttons/Logout.jsx
@@ -1,8 +1,8 @@
 import logoutIcon from '../../../../assets/icons/icons8-logout-48.png'
 
-export const Logout = () => {
-  return (
-		<div className='logout'>
+export const Logout = ({ logout }) => {
+	return (
+		<div onClick={logout} className='logout'>
 			<img src={logoutIcon} alt='logout-icon' />
 			<span>Logout</span>
 		</div>
