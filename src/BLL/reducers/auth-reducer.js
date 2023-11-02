af1362344f6ad963a0ba614b81119cc46b6cb06a
crossroads30 <crossroads30@mail.ru>
2023-11-02 16:21:24 +0300

    feat: add set captchaUrl AC & thunk, add if else

--- src/BLL/reducers/auth-reducer.js
+++ src/BLL/reducers/auth-reducer.js
@@ -2,6 +2,7 @@ import { stopSubmit } from 'redux-form'
 import { authApi } from '../../DAL/api'
 
 const SET_USER_AUTH_DATA = 'auth/SET_USER_AUTH_DATA'
+const SET_CAPTCHA_URL_SUCCESS = 'auth/SET_CAPTCHA_URL_SUCCESS'
 
 let initialState = {
 	id: null,
@@ -13,6 +14,7 @@ let initialState = {
 const authReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case SET_USER_AUTH_DATA:
+		case SET_CAPTCHA_URL_SUCCESS:
 			return {
 				...state,
 				...action.data,
@@ -22,11 +24,18 @@ const authReducer = (state = initialState, action) => {
 	}
 }
 
+
 //actionCreators:
 export const setUserAuthData = (id, login, email, isAuth) => ({
 	type: SET_USER_AUTH_DATA,
 	data: { id, login, email, isAuth },
 })
+
+export const setCaptchaUrlSuccess = captchaUrl => ({
+	type: SET_USER_AUTH_DATA,
+	data:{ captchaUrl },
+})
+
 
 //ThunkCreators:
 export const getAuthUserData = () => async dispatch => {
@@ -38,18 +47,36 @@ export const getAuthUserData = () => async dispatch => {
 		console.log(error)
 	}
 }
+
 export const getLoginToSystem = (email, password, rememberMe, captcha) => async dispatch => {
 	try {
 		const response = await authApi.loginToServer(email, password, rememberMe, captcha)
-		response.data.resultCode === 0 && dispatch(getAuthUserData())
+		if(response.data.resultCode === 0) {
+			dispatch(getAuthUserData())
+		} else {
+			if (response.data.resultCode === 10) {
+				dispatch(getCaptchaUrl())
+			}
+		}
 	} catch (error) {
 		console.log(error)
 	}
 }
+
 export const getLogoutFromSystem = () => async dispatch => {
 	try {
 		const response = await authApi.logOutFromServer()
 		response.data.resultCode === 0 && dispatch(setUserAuthData(null, null, null, false))
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+export const getCaptchaUrl = url => async dispatch => {
+	try {
+		const response = await securityApi.getCaptchaUrl()
+		const captchaUrl = response.data.url
+		response.data.resultCode === 0 && dispatch(setCaptchaUrlSuccess(captchaUrl))
 	} catch (error) {
 		console.log(error)
 	}
