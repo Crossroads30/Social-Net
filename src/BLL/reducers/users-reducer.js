4efafbecdb74be4096519dc23d5f93f92be73f32
crossroads30 <crossroads30@mail.ru>
2023-11-03 11:14:50 +0300

    feat: create usersReducer

---
+++ src/BLL/reducers/users-reducer.js
@@ -0,0 +1,17 @@
+let initialState = {
+	users: [],
+}
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+		default:
+			return state
+	}
+
+}
+
+//actionCreators:
+
+//ThunkCreators:
+
+export default userReducer
