38ab68ce635d8358e46e30b08e5b72bf30a5d211
crossroads30 <crossroads30@mail.ru>
2023-11-03 13:50:10 +0300

    feat: add case add AC, add thunk for users request

--- src/BLL/reducers/users-reducer.js
+++ src/BLL/reducers/users-reducer.js
@@ -1,17 +1,37 @@
+import { usersApi } from '../../DAL/api'
+
+const SET_USERS = 'users-reducer/SET_USERS'
+
 let initialState = {
 	users: [],
 }
 
 const userReducer = (state = initialState, action) => {
-  switch (action.type) {
+	switch (action.type) {
+		case SET_USERS:
+			return {
+				...state,
+				users: action.users,
+			}
 		default:
 			return state
 	}
-
 }
 
 //actionCreators:
+export const setUsers = users => ({
+	type: SET_USERS,
+	users,
+})
 
 //ThunkCreators:
-
+export const getAllUsers = () => async dispatch => {
+	try {
+		const response = await usersApi.getUsers()
+		dispatch(setUsers(response))
+		console.log(response)
+	} catch (error) {
+		console.log(error)
+	}
+}
 export default userReducer
