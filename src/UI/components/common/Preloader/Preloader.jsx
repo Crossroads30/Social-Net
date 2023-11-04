2b9fa116e01d278560738b2fe359cfa34138229e
crossroads30 <crossroads30@mail.ru>
2023-11-04 19:27:06 +0300

    create component preloader

---
+++ src/UI/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,7 @@
+import preloaderGifUrl from '../../../../assets/gif/colorful-loader-gif-transparent.gif'
+
+const Preloader = () => {
+  return <img className='preloader' src={preloaderGifUrl} alt='preloader' />
+}
+
+export default Preloader
