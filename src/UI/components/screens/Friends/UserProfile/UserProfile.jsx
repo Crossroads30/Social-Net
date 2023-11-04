1b3b4abed48e86d256762f26fa358dae3a65b595
crossroads30 <crossroads30@mail.ru>
2023-11-04 10:35:51 +0300

    feat: add user name to upperCase

--- src/UI/components/screens/Friends/UserProfile/UserProfile.jsx
+++ src/UI/components/screens/Friends/UserProfile/UserProfile.jsx
@@ -8,7 +8,7 @@ const UserProfile = ({ user }) => {
 				<img className={cl.userAvatar} src={user.photos.small ? user.photos.small : defaultUserImg} alt='Avatar' />
 			</div>
 			<div className={cl.userAvatarWrapper}>
-				<div className={cl.userName}>{user.name}</div>
+				<div className={cl.userName}>{user.name.charAt(0).toUpperCase() + user.name.slice(1)}</div>
 				<div className={cl.userStatus}>{user.status === null ? 'No status' : user.status}</div>
 			</div>
 		</div>
