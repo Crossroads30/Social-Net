5972ddc866d1fb84ec72957014286f1afa1d6d93
crossroads30 <crossroads30@mail.ru>
2023-11-04 16:11:42 +0300

    feat: add props

--- src/UI/components/screens/Friends/Friends.jsx
+++ src/UI/components/screens/Friends/Friends.jsx
@@ -2,10 +2,10 @@ import Paginator from '../../common/Paginator/Paginator'
 import cl from './Friends.module.css'
 import UserInfo from './UserInfo/UserInfo'
 
-const Friends = ({ allUsers, currentPage, pageSize, totalUsersCount }) => {
+const Friends = ({ allUsers, currentPage, pageSize, totalUsersCount, onPageChangeHandler }) => {
 	return (
 		<div className={cl.friendsWrapper}>
-			<Paginator totalUsersCount={totalUsersCount} allUsers={allUsers} pageSize={pageSize} />
+			<Paginator totalUsersCount={totalUsersCount} allUsers={allUsers} pageSize={pageSize} onPageChangeHandler={onPageChangeHandler} />
 			<div className={cl.usersContainer}>
 				{allUsers.map(user => (
 					<UserInfo user={user} currentPage={currentPage} pageSize={pageSize} key={user.id} />
