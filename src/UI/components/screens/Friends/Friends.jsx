7d5991261f5f5a4c2900091dd98500d82220b24c
crossroads30 <crossroads30@mail.ru>
2023-11-04 15:30:13 +0300

    feat: add pagination to page

--- src/UI/components/screens/Friends/Friends.jsx
+++ src/UI/components/screens/Friends/Friends.jsx
@@ -1,12 +1,16 @@
+import Paginator from '../../common/Paginator/Paginator'
 import cl from './Friends.module.css'
 import UserInfo from './UserInfo/UserInfo'
 
-const Friends = ({ allUsers, currentPage, pageSize }) => {
+const Friends = ({ allUsers, currentPage, pageSize, totalUsersCount }) => {
 	return (
-		<div className={cl.usersContainer}>
-			{allUsers.map(user => (
-				<UserInfo user={user} currentPage={currentPage} pageSize={pageSize} key={user.id} />
-			))}
+		<div className={cl.friendsWrapper}>
+			<Paginator totalUsersCount={totalUsersCount} allUsers={allUsers} pageSize={pageSize} />
+			<div className={cl.usersContainer}>
+				{allUsers.map(user => (
+					<UserInfo user={user} currentPage={currentPage} pageSize={pageSize} key={user.id} />
+				))}
+			</div>
 		</div>
 	)
 }
