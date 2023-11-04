dfb7bd582d6161f5c6eb86604ba8d78948aef38e
crossroads30 <crossroads30@mail.ru>
2023-11-04 16:26:37 +0300

    feat: add pagination to the end of users list

--- src/UI/components/screens/Friends/Friends.jsx
+++ src/UI/components/screens/Friends/Friends.jsx
@@ -5,12 +5,25 @@ import UserInfo from './UserInfo/UserInfo'
 const Friends = ({ allUsers, currentPage, pageSize, totalUsersCount, onPageChangeHandler }) => {
 	return (
 		<div className={cl.friendsWrapper}>
-			<Paginator totalUsersCount={totalUsersCount} allUsers={allUsers} pageSize={pageSize} onPageChangeHandler={onPageChangeHandler} />
+			<Paginator
+				totalUsersCount={totalUsersCount}
+				allUsers={allUsers}
+				pageSize={pageSize}
+				onPageChangeHandler={onPageChangeHandler}
+				currentPage={currentPage}
+			/>
 			<div className={cl.usersContainer}>
 				{allUsers.map(user => (
 					<UserInfo user={user} currentPage={currentPage} pageSize={pageSize} key={user.id} />
 				))}
 			</div>
+			<Paginator
+				totalUsersCount={totalUsersCount}
+				allUsers={allUsers}
+				pageSize={pageSize}
+				onPageChangeHandler={onPageChangeHandler}
+				currentPage={currentPage}
+			/>
 		</div>
 	)
 }
