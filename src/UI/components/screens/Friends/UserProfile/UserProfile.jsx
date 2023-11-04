0dc11f1381b0284b64153447475d00da85f6adb2
crossroads30 <crossroads30@mail.ru>
2023-11-04 10:04:44 +0300

    feat: add classNames for elements

--- src/UI/components/screens/Friends/UserProfile/UserProfile.jsx
+++ src/UI/components/screens/Friends/UserProfile/UserProfile.jsx
@@ -3,12 +3,14 @@ import cl from './UserProfile.module.css'
 
 const UserProfile = ({ user }) => {
 	return (
-		<div>
-			<div>
+		<div className={cl.userProfileWrapper}>
+			<div className={cl.userAvatarWrapper}>
 				<img className={cl.userAvatar} src={user.photos.small ? user.photos.small : defaultUserImg} alt='Avatar' />
 			</div>
-			<div>{user.name}</div>
-			<div>{user.status === null ? 'No status' : user.status}</div>
+			<div className={cl.userAvatarWrapper}>
+				<div className={cl.userName}>{user.name}</div>
+				<div className={cl.userStatus}>{user.status === null ? 'No status' : user.status}</div>
+			</div>
 		</div>
 	)
 }
