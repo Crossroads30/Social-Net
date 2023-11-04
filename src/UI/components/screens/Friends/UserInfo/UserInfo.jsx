c83c396f68be6b7df22ffdfdc8d1612ddb0b79b0
crossroads30 <crossroads30@mail.ru>
2023-11-04 11:55:55 +0300

    refactor: rename component

---
+++ src/UI/components/screens/Friends/UserInfo/UserInfo.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from 'react-router-dom'
+import defaultUserImg from '../../../../../assets/icons/default-user-img.png'
+import FollowUnfollowButton from '../../../common/buttons/FollowButton'
+import cl from '../Friends.module.css'
+
+const UserInfo = ({ user }) => {
+	return (
+		<NavLink to={'/profile/' + user.id} className={cl.userProfileWrapper}>
+			<div>
+				<img className={cl.userAvatar} src={user.photos.small ? user.photos.small : defaultUserImg} alt='Avatar' />
+				<div className={cl.userName}>{user.name.charAt(0).toUpperCase() + user.name.slice(1)}</div>
+				<div className={cl.userStatus}>{user.status === null ? 'No status' : user.status}</div>
+				<FollowUnfollowButton name={'Follow'} />
+			</div>
+		</NavLink>
+	)
+}
+
+export default UserInfo
