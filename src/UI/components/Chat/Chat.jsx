8c328fb53fe7409c38d0310ef5931dad547603fb
crossroads30 <crossroads30@mail.ru>
2023-11-06 21:49:05 +0300

    feat: add footer

--- src/UI/components/Chat/Chat.jsx
+++ src/UI/components/Chat/Chat.jsx
@@ -1,11 +1,12 @@
 import classNames from 'classnames'
 import Footer from './Footer/Footer'
+import cl from './Chat.module.css'
 
 const Chat = () => {
 	return (
 		<div className='chat-wrapper'>
 			<div className={classNames('box-wrapper', 'box-wrapper2')}>
-				<h2>chat</h2>
+				<div className={cl.friendsChat}>Friends chat</div>
 				<Footer />
 			</div>
 		</div>
--- src/UI/components/Chat/Footer/Footer.jsx
+++ src/UI/components/Chat/Footer/Footer.jsx
@@ -4,8 +4,11 @@ import logo from '../../../../assets/icons/calypso.svg'
 const Footer = () => {
 	return (
 		<div className={cl.footer}>
-    <img src={logo} alt="logo" />
-			<p>Calypso Corporation © 2023</p>
+			<div>
+				<img src={logo} alt='logo' />
+				<p>Calypso Corporation © 2023</p>
+			</div>
+			<p>All rights reserved</p>
 		</div>
 	)
 }
