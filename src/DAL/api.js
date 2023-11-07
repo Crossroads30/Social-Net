25ab2d450550ab8d814f73c0c2c456d7c30710f2
crossroads30 <crossroads30@mail.ru>
2023-11-07 16:06:35 +0300

    feat: add request for friends

--- src/DAL/api.js
+++ src/DAL/api.js
@@ -65,4 +65,7 @@ export const usersApi = {
 	async getUnfollow(userId) {
 		return await instance.delete(`/follow/${userId}`).then(response => response.data)
 	},
+	async getFollowedFriends(totalUsersCount = 30) {
+		return await instance.get(`users?friend=true&count=${totalUsersCount}`).then(response => response.data.items)
+	},
 }
